0ed2864b286238f9fa71597d6d9c68c9d10c7b0e
Edgar Burelo <edgar.burelo@gmail.com>
2018-09-14 16:39:58 -0500

    add still status and animate

--- assets/back.js
+++ assets/back.js
@@ -20,13 +20,21 @@ $(".button").on("click", function() {
           var p = $("<p>").text("Rating: " + rating);
 
           var personImage = $("<img>");
-          personImage.attr("src", results[i].images.fixed_height.url);
-
+          personImage.attr("src", results[i].images.fixed_height_still.url);
+          personImage.attr("data-still",results[i].images.fixed_height_still.url);
+          personImage.attr("data-animate",results[i].images.fixed_height.url);
+          personImage.attr("data-state","still");
+          personImage.attr("class","gif");
+          
+        //   <img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" data-state="still" class="gif"></img>
+        
           gifDiv.prepend(p);
           gifDiv.prepend(personImage);
 
           $("#gifs-appear-here").prepend(gifDiv);
+          
         }
+        aniGif();
       });
   });
 };
@@ -52,6 +60,21 @@ $(".button").on("click", function() {
     $("#btnhld").append(b);
     });
 
+  };
+  function aniGif() {
+    $(".gif").on("click", function() {
+        
+        var state = $(this).attr("data-state");
+        console.log(state);
+        
+        if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+        } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+        }
+    });
   };
 
   
